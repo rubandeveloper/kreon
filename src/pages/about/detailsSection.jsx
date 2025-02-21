a64c1fed918b653337e6911dd3e2f66a26f52f01
rubandeveloper <rajajayarubant@gmail.com>
2025-02-22 00:42:23 +0530

    merge

--- src/pages/about/detailsSection.jsx
+++ src/pages/about/detailsSection.jsx
@@ -1,123 +1,120 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 // Assets
-import './style.css';
-import Images from '../../assets/Images';
-import Icons from '../../assets/Icons';
-
+import "./style.css";
+import Images from "../../assets/Images";
+import Icons from "../../assets/Icons";
 
 // Components
-import Button from '../../components/Button';
+import Button from "../../components/Button";
 
 const DetailsSection = () => {
-
-    const [DetailsContent, setDetailsContent] = useState({
-        id: 'details-content-cards',
-        title: `Kreon Financial's Guiding <br /> Principles and Core Values`,
-        items: [
-            {
-                icon: Images.icons.customer,
-                title: 'Customer-centric',
-                desc: `
+  const [DetailsContent, setDetailsContent] = useState({
+    id: "details-content-cards",
+    title: `Kreon Finnancial's Guiding <br /> Principles and Core Values`,
+    items: [
+      {
+        icon: Images.icons.customer,
+        title: "Customer-centric",
+        desc: `
                     Our customers are at the heart of everything we do. 
                     We strive to understand their unique needs and provide 
                     personalized financial solutions that truly make a difference in their lives.
-                `
-            },
-            {
-                icon: Images.icons.integrity,
-                title: 'Integrity',
-                desc: `
+                `,
+      },
+      {
+        icon: Images.icons.integrity,
+        title: "Integrity",
+        desc: `
                     Trust is our cornerstone. We uphold the highest standards of transparency, 
                     honesty, and ethical practices, ensuring that every interaction builds lasting relationships.
-                `
-            },
-            {
-                icon: Images.icons.inclusivity_icon,
-                title: 'Inclusivity',
-                desc: `
+                `,
+      },
+      {
+        icon: Images.icons.inclusivity_icon,
+        title: "Inclusivity",
+        desc: `
                     We believe everyone deserves access to financial opportunities. By bridging gaps 
                     and empowering underserved communities, we create a more equitable financial ecosystem.
-                `
-            },
-            {
-                icon: Images.icons.excellence,
-                title: 'Excellence',
-                desc: `
+                `,
+      },
+      {
+        icon: Images.icons.excellence,
+        title: "Excellence",
+        desc: `
                     Excellence defines our work. From seamless user experiences to robust financial 
                     products, we go above and beyond to deliver value that exceeds expectations.
-                `
-            },
-            {
-                icon: Images.icons.customer,
-                title: 'Sustainability',
-                desc: `
+                `,
+      },
+      {
+        icon: Images.icons.customer,
+        title: "Sustainability",
+        desc: `
                     Beyond profits, we’re dedicated to making a lasting impact. 
                     Through responsible lending and financial education, we aim 
                     to foster growth and resilience for individuals and communities alike.
-                `
-            },
-            {
-                icon: Images.icons.collaboration,
-                title: 'Collaboration',
-                desc: `
+                `,
+      },
+      {
+        icon: Images.icons.collaboration,
+        title: "Collaboration",
+        desc: `
                     Together, we achieve more. By working closely with our customers, 
                     partners, and employees, we create solutions that drive shared 
                     success and mutual grow
-                `
-            },
-        ]
-    })
+                `,
+      },
+    ],
+  });
 
-
-    
-    return (
-        <div className='tablesection-container-main'
-            style={{ 
-                minHeight: 'max-content', 
-                backgroundImage:`url(${Images.bottom_curve})`,
-                backgroundSize: '100%',
-                backgroundPosition: 'top',
-                backgroundPositionY: '-0%',
-                paddingTop: '15%', 
-                paddingBottom: '10%', 
-
-            }}
-        >
-            <div className="tablesection-container-content">
-                <div className="tablesection-head">
-                    <img className="tablesection-logo" src={Images.logo_border} />
-                    <h1 className="tablesection-title" dangerouslySetInnerHTML={{__html: DetailsContent.title}}></h1>
-
-                </div>
-
-                <div className="tablesection-details_cards-section">
-                    <div className="details_cards-section-items">
-                        {DetailsContent.items.map((item, idx)=>(
-                            <div 
-                                key={`details_cards-item-${idx}`}
-                                className="details_cards-section-item"
-                            >
-                                <img className='item-icon' src={item.icon} />
-                                <div className="item-title">{item.title}</div>
-                                <div className="item-desc">{item.desc}</div>
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className="details_cards-actions">
-                        <Button
-                            key={`contact-us-btn`}
-                            id='contact-us-btn'
-                            label="CONTACT US"
-                            callback={() => { }}
-                        />
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div
+      className="tablesection-container-main"
+      style={{
+        minHeight: "max-content",
+        backgroundImage: `url(${Images.bottom_curve})`,
+        backgroundSize: "100%",
+        backgroundPosition: "top",
+        backgroundPositionY: "-0%",
+        paddingTop: "15%",
+        paddingBottom: "10%",
+      }}
+    >
+      <div className="tablesection-container-content">
+        <div className="tablesection-head">
+          <img className="tablesection-logo" src={Images.logo_border} />
+          <h1
+            className="tablesection-title"
+            dangerouslySetInnerHTML={{ __html: DetailsContent.title }}
+          ></h1>
         </div>
-    )
-}
+
+        <div className="tablesection-details_cards-section">
+          <div className="details_cards-section-items">
+            {DetailsContent.items.map((item, idx) => (
+              <div
+                key={`details_cards-item-${idx}`}
+                className="details_cards-section-item"
+              >
+                <img className="item-icon" src={item.icon} />
+                <div className="item-title">{item.title}</div>
+                <div className="item-desc">{item.desc}</div>
+              </div>
+            ))}
+          </div>
+
+          <div className="details_cards-actions">
+            <Button
+              key={`contact-us-btn`}
+              id="contact-us-btn"
+              label="CONTACT US"
+              callback={() => {}}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DetailsSection;
--- src/pages/about/servicesSection.jsx
+++ src/pages/about/servicesSection.jsx
@@ -1,112 +1,153 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 // Assets
-import './style.css';
-import Images from '../../assets/Images';
-import Icons from '../../assets/Icons';
-
+import "./style.css";
+import Images from "../../assets/Images";
+import Icons from "../../assets/Icons";
 
 // Components
-import Button from '../../components/Button';
+import Button from "../../components/Button";
 
 const ServicesSection = () => {
-
-    const [Services, setServices] = useState([
+  const [Services, setServices] = useState([
+    {
+      title: "Salary Credit",
+      desc: "An Earned Wage Access platform providing professionals instant access to earned wages, promoting financial wellness and reducing reliance on costly borrowing.",
+      banner: Images.salary_credit,
+      ratings: [
+        { label: "1M+ Downloads" },
+        { label: "4.6", icon: Icons.star },
+        { label: "60k+ Reviews" },
+      ],
+      buttons: [
         {
-            title: 'Salary Credit',
-            desc: 'An Earned Wage Access platform providing professionals instant access to earned wages, promoting financial wellness and reducing reliance on costly borrowing.',
-            banner: Images.salary_credit,
-            ratings: [
-                { label: '1M+ Downloads' },
-                { label: '4.6', icon: Icons.star },
-                { label: '60k+ Reviews' }
-            ],
-            buttons: [
-                { label: 'DOWNLOAD APP', icon: Icons.playstore, iconAlign: 'right', callback: () => { }, customStyle: { width: '100%' } },
-                { label: 'VISIT SITE', icon: Icons.link, iconAlign: 'right', callback: () => { }, type: 'secondary', customStyle: { width: '100%' } }
-            ],
+          label: "DOWNLOAD APP",
+          icon: Icons.playstore,
+          iconAlign: "right",
+          callback: () => {
+            window.open(
+              "https://play.google.com/store/apps/details?id=com.mdpd"
+            );
+          },
+          customStyle: { width: "100%" },
         },
         {
-            title: 'Student Credit',
-            desc: 'A digital lending platform providing instant overdraft facilities to college students across India, promoting formal nancial inclusion and credit discipline.',
-            banner: Images.student_credit,
-            ratings: [
-                { label: '1M+ Downloads' },
-                { label: '4.6', icon: Icons.star },
-                { label: '60k+ Reviews' }
-            ],
-            buttons: [
-                { label: 'DOWNLOAD APP', icon: Icons.playstore, iconAlign: 'right', callback: () => { }, customStyle: { width: '100%' } },
-                { label: 'VISIT SITE', icon: Icons.link, iconAlign: 'right', callback: () => { }, type: 'secondary', customStyle: { width: '100%' } }
-            ],
+          label: "VISIT SITE",
+          icon: Icons.link,
+          iconAlign: "right",
+          callback: () => {
+            window.open("https://kreon.in/");
+          },
+          type: "secondary",
+          customStyle: { width: "100%" },
+        },
+      ],
+    },
+    {
+      title: "Student Credit",
+      desc: "A digital lending platform providing instant overdraft facilities to college students across India, promoting formal nancial inclusion and credit discipline.",
+      banner: Images.student_credit,
+      ratings: [
+        { label: "1M+ Downloads" },
+        { label: "4.6", icon: Icons.star },
+        { label: "60k+ Reviews" },
+      ],
+      buttons: [
+        {
+          label: "DOWNLOAD APP",
+          icon: Icons.playstore,
+          iconAlign: "right",
+          callback: () => {
+            window.open(
+              "https://play.google.com/store/apps/details?id=com.kreon.stucred"
+            );
+          },
+          customStyle: { width: "100%" },
         },
         {
-            title: 'Traditional Lending',
-            desc: 'We offer tailored loans with flexible terms and competitive rates, supporting personal growth, business expansion, and financial stability with trust and transparency.',
-            banner: Images.lending,
-            ratings: [
-                { label: '₹4000Cr+  Disbursed' },
-                { label: 'Transparent Terms' },
-                { label: '1M+ Satisfied Users' },
-                { label: 'Rapid Disbursals' },
-            ],
-            buttons: [
-                { label: 'GET MY LOAN', icon: Icons.playstore, iconAlign: 'right', callback: () => { }, customStyle: { width: '100%' } },
-            ],
+          label: "VISIT SITE",
+          icon: Icons.link,
+          iconAlign: "right",
+          callback: () => {
+            window.open("https://stucred.com/");
+          },
+          type: "secondary",
+          customStyle: { width: "100%" },
         },
-    ])
+      ],
+    },
+    {
+      title: "Traditional Lending",
+      desc: "We offer tailored loans with flexible terms and competitive rates, supporting personal growth, business expansion, and financial stability with trust and transparency.",
+      banner: Images.lending,
+      ratings: [
+        { label: "₹4000Cr+  Disbursed" },
+        { label: "Transparent Terms" },
+        { label: "1M+ Satisfied Users" },
+        { label: "Rapid Disbursals" },
+      ],
+      buttons: [
+        {
+          label: "GET MY LOAN",
+          icon: Icons.playstore,
+          iconAlign: "right",
+          callback: () => {},
+          customStyle: { width: "100%" },
+        },
+      ],
+    },
+  ]);
 
-
-
-    return (
-        <div className='services-container-main'>
-            <div className="services-container-content">
-                <div className="services-head">
-                    <img className="services-logo" src={Images.logo_border} />
-                    <h1 className="services-title">Our Services</h1>
-
-                </div>
-
-                <div className="services-content-main">
-                    <div className="services-content-items">
-                        {Services.map((service, index) => (
-                            <div className="services-content-item">
-                                <div className="title">{service.title}</div>
-                                <div className="desc">{service.desc}</div>
-                                <img className='banner' src={service.banner} />
-
-                                <div className="item-rating-items">
-                                    {service.ratings.map((rating, index) => (
-                                        <div className="item-rating-item">
-                                            <div className="label">{rating.label}</div>
-                                            {rating.icon && <div className="icon" dangerouslySetInnerHTML={{ __html: rating.icon }}></div>}
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="item-buttons">
-                                    {service.buttons.map((button, index) => (
-                                        <Button
-                                            key={`button-${index}`}
-                                            id={`button-${index}`}
-                                            label={button.label}
-                                            icon={button.icon}
-                                            iconAlign={button.iconAlign}
-                                            callback={button.callback}
-                                            type={button.type}
-                                            customStyle={button.customStyle}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-
-                    </div>
-                </div>
-
-
-            </div>
+  return (
+    <div className="services-container-main">
+      <div className="services-container-content">
+        <div className="services-head">
+          <img className="services-logo" src={Images.logo_border} />
+          <h1 className="services-title">Our Services</h1>
         </div>
-    )
-}
+
+        <div className="services-content-main">
+          <div className="services-content-items">
+            {Services.map((service, index) => (
+              <div className="services-content-item">
+                <div className="title">{service.title}</div>
+                <div className="desc">{service.desc}</div>
+                <img className="banner" src={service.banner} />
+
+                <div className="item-rating-items">
+                  {service.ratings.map((rating, index) => (
+                    <div className="item-rating-item">
+                      <div className="label">{rating.label}</div>
+                      {rating.icon && (
+                        <div
+                          className="icon"
+                          dangerouslySetInnerHTML={{ __html: rating.icon }}
+                        ></div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+                <div className="item-buttons">
+                  {service.buttons.map((button, index) => (
+                    <Button
+                      key={`button-${index}`}
+                      id={`button-${index}`}
+                      label={button.label}
+                      icon={button.icon}
+                      iconAlign={button.iconAlign}
+                      callback={button.callback}
+                      type={button.type}
+                      customStyle={button.customStyle}
+                    />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ServicesSection;
